bfdd799d87c19b5b89fc6a23871f7eab9ce727e3
Operator-One <148469445+Operator-One@users.noreply.github.com>
2024-02-13 02:06:01 -0500

    Add files via upload

--- start-spigot-mc.sh
+++ start-spigot-mc.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-echo "Starting the Minecraft Server"
-java -jar -Xmx4G -Xmx2G -XX:+UseZGC $HOME/minecraft-server/spigot.jar &
-first_pid=$!
-
-# Start the second program in the background
-echo "Starting Bedrock Connect"
-java -jar -Xmx4G -Xmx2G -XX:+UseZGC $HOME/minecraft-server/bedrock-connect.jar &
-second_pid=$!
-
-# Wait for all background processes to exit
-wait $first_pid
-wait $second_pid
+nohup java -jar -Xmx4G -Xmx2G -XX:+UseZGC $HOME/minecraft-server/spigot.jar > spigot.log 2>&1 &
+nohup java -jar $HOME/minecraft-server/bedrock-connect.jar nodb=true > bedrock-connect.log 2>&1 &
