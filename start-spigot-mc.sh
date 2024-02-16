e2e4d321081c028c56fc57d6dc001443eeb89c82
Operator-One <cullen.guimond@guinet.us>
2024-02-16 14:36:23 -0500

    Further removing $HOME from scriptfiles

--- start-spigot-mc.sh
+++ start-spigot-mc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nohup java -jar -Xmx4G -Xmx2G -XX:+UseZGC $HOME/minecraft-server/spigot.jar > spigot.log 2>&1 &
-nohup java -jar $HOME/minecraft-server/bedrock-connect.jar nodb=true > bedrock-connect.log 2>&1 &
+nohup java -jar -Xmx4G -Xmx2G -XX:+UseZGC /home/serveruser/minecraft-server/spigot.jar > spigot.log 2>&1 &
+nohup java -jar /home/serveruser/minecraft-server/bedrock-connect.jar nodb=true > bedrock-connect.log 2>&1 &
