f0f38f43ea428a1bea0112b30a2271a5725f0dd5
Operator-One <cullen.guimond@guinet.us>
2024-02-16 14:40:10 -0500

    Changing path of Java executables in start script

--- start-spigot-mc.sh
+++ start-spigot-mc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nohup java -jar -Xmx4G -Xmx2G -XX:+UseZGC /home/serveruser/minecraft-server/spigot.jar > spigot.log 2>&1 &
-nohup java -jar /home/serveruser/minecraft-server/bedrock-connect.jar nodb=true > bedrock-connect.log 2>&1 &
+nohup /etc/alternatives/jre_17_openjdk/bin/java -jar -Xmx4G -Xmx2G -XX:+UseZGC /home/serveruser/minecraft-server/spigot.jar > spigot.log 2>&1 &
+nohup /etc/alternatives/jre_1.8.0/bin/java -jar /home/serveruser/minecraft-server/bedrock-connect.jar nodb=true > bedrock-connect.log 2>&1 &
