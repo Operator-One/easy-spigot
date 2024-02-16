3de9f6fb25a96f1620ae76d4ef46818d2f8473b1
Operator-One <cullen.guimond@guinet.us>
2024-02-16 14:41:24 -0500

    Changing more java executables in build script

--- build-server.sh
+++ build-server.sh
@@ -79,10 +79,10 @@ echo "Download completed: BuildTools.jar"
 #Build spigot
 cd /home/serveruser/minecraft-server
 echo "Building/configuring spigot, this process will take a few minutes to complete"
-java -jar -Xmx2G -Xms2G -XX:+UseZGC BuildTools.jar
+/etc/alternatives/jre_17_openjdk/bin/java -jar -Xmx2G -Xms2G -XX:+UseZGC BuildTools.jar
 mv spigot-*.jar /home/serveruser/minecraft-server/spigot.jar
 echo "eula=true" > eula.txt 
-java -jar -Xmx2G -Xms2G -XX:+UseZGC /home/serveruser/minecraft-server/spigot.jar &
+/etc/alternatives/jre_17_openjdk/bin/java -jar -Xmx2G -Xms2G -XX:+UseZGC /home/serveruser/minecraft-server/spigot.jar &
 sleep 5
 JAVA_PID=$(jobs -p | tail -n 1)
 sleep 60
